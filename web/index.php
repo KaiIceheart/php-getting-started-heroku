cc89b187d209962f8048bba2d8d55328a4cfee28
KaiIceheart <35499821+KaiIceheart@users.noreply.github.com>
2018-03-13 18:45:18 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,44 +1,129 @@
 <!DOCTYPE html>
-<html lang="en"><head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Form test</title>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="CSS/normalize.css">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/media.css">
+    <title>ritual dezign</title>
 </head>
 
 <body>
+    <ul class="m-menu">
+        <li class="close-menu">&times;</li>
+        <li><a href="#" class="menu-link">Главная</a></li>
+        <li><a href="#" class="menu-link">Почему Анхель</a></li>
+         <li><a href="#" class="menu-link">Цены на гробы</a></li>
+        <li><a href="#" class="menu-link">Цены на надгробия</a></li>
+        <li><a href="#" class="get-started-btn menu-link">Заказать</a></li>
+    </ul>
+    <div class="main">
+        <div class="main-overlay"></div>
+        <div class="header">
+            <div class="logo">
+                <img src="img/Logo.png" alt="Логотип">
+            </div>
+            <a href="#" class="m-menu-link">
+					<i class="fa fa-bars"></i>
+				</a>
+            <div class="menu visible-lg">
+                <ul>
+                    <li><a href="#" class="menu-link">Главная</a></li>
+                    <li><a href="#" class="menu-link">Почему Анхель</a></li>
+                     <li><a href="#" class="menu-link">Цены на гробы</a></li>
+        <li><a href="#" class="menu-link">Цены на надгробия</a></li>
+                    <li><a href="#" class="get-started-btn menu-link">Заказать</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="main-text">
+            <span>АНХЕЛЬ - С НАМИ В ПОСЛЕДНИЙ ПУТЬ</span>
+            <h1>Ритуальный дизайн</h1>
+            <br>
+            <br>
+            <span>«ПОХОРОННОЕ ИСКУССТВО»</span>
+        </div>
+        <a href="#plan" class="main-arrow">
+            <div class="arrow-down">
+                <img src="img/arrow.png" alt="Arrow">
+            </div>
+        </a>
+    </div>
+    <div id="plan" class="table">
+        <div class="table-header">
+            <span>АНХЕЛЬ</span>
+            <h2>Выберите Услугу</h2>
+            <p>Эксклюзивный дизайн и оформление. Индивидуальность и креатив.</p>
+        </div>
+        <div class="price-tags">
+            <div class="price-tag">
+                <img src="img/table/Asset 1@4x.png" alt="">
+                <br>
+                <br>
+                <span>дизайн гробов</span>
+                <h3>от 500<sup>$</sup></h3>
+                <p>
+                    Хотите выгодно отличиться от других. Крутая машина, шикарный дом... Осталось заказать при жизни гроб в соответствии со своим характером: классический и роскошный или современный и динамичный. Гроб на заказ в любом стиле.</p>
+                <button class="price-btn">выбрать услугу</button>
+            </div>
+            <div class="price-tag">
+                <img src="img/table/Asset 2@4x.png" alt="">
+                <span>дизайн надгробий</span>
+                <h3>от 700<sup>$</sup></h3>
+                <p>Мы занимаемся разработкой и производством специальных надгробий оснащенных радиоэлектронными системами для получения информации о захороненном человеке: фото, биография, ссылка на сайт памяти и т.д. </p>
+                <button class="price-btn">выбрать услугу</button>
+            </div>
+            <div class="price-tag">
+                <img src="img/table/Asset 3@4x.png" alt="">
+                <span>похоронный дизайн</span>
+                <h3>от 1200<sup>$</sup></h3>
+                <p>Важной частью прощания с близким человеком, является поминальный обед. Тематическое оформление и сервировка стола подчеркивают высокий статус похоронного приема.</p>
+                <button class="price-btn">выбрать услугу</button>
+            </div>
+        </div>
+    </div>
 
-           <?php if (isset($_GET['submitted'])) { ?>
-    <h1>The server had accepted:</h1>
-    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
-    <p>Login: <?php echo htmlspecialchars($_GET['Login']); ?></p>
-    <p>Color: <?php echo htmlspecialchars($_GET['Color']); 
-                               <span style="background-color: <?php echo htmlspecialchars($_GET['Color']); ?>;        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span></p>
-    <p>Try again? <a href="/index.php">Yes!</a></p>
-  <?php } ?>
-  <?php if (!isset($_GET['submitted'])) { ?>
-               <h1>Play with the HTML form</h1>
-                <form action="/index.php">
-                    <input type="hidden" name="submitted" value="yes">
-                    <p>
-                        <label for="my-radio-yes">radio Yes: </label>
-                        <input id="my-radio-yes" name="radio" type="radio" required="" value="Yes">
-                        <label for="my-radio-no">radio No: </label>
-                        <input id="my-radio-no" name="radio" type="radio" required="" value="No"></p>
-                   
-                    <label for="Login">Login: </label>
-                    <input id="Login" name="Login" type="text" required="" value="text">
-                    <button type="submit">
-                            <br>
-                            <b>Let`s go</b><br>
-                          </button>
-                    <br>
-                    <label for="Color">Color </label>
-                    <input id="Color" name="Color" type="color" required="" value="Color">
-                    <br>
-                    <br>
-                    <input type="reset" value="Clear">
-                    <p></p>
-                </form>
- <?php } ?>           
-</body></html>
+    <div class="call-to-action">
+        <div class="call-to-action-overlay"></div>
+        <div class="wrap">
+            <h4>ЧТО БУДЕТ ЗАВТРА?<br> ПЛАНИРУЙ НАПЕРЕД!</h4>
+            <button class="cta-btn">оформить услугу</button>
+            <button class="cta-btn">информация о нас</button>
+        </div>
+    </div>
+    <div class="footer-wrap">
+        <div class="footer">
+            <div class="logo">
+                <img src="img/Logo-bl.png" alt="Logo">
+            </div>
+            <div class="footer-menu">
+                <ul>
+                    <li><a href="#" class="footer-menu-link">Главная</a></li>
+                    <li><a href="#" class="footer-menu-link">Почему Анхель</a></li>
+                    <li><a href="#" class="footer-menu-link">Цены на гробы</a></li>
+                    <li><a href="#" class="footer-menu-link">Цены на надгробия</a></li>
+                    <li><a href="#" class="footer-get-started footer-menu-link">Заказать</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="line"></div>
+    <div class="legal">
+        <div class="copy">
+            <p>© 2018 Сделано с любовью для Вас. Богдан Янушкевич</p>
+        </div>
+        <div class="social-ico">
+            <a href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
+            <a href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
+            <a href="https://plus.google.com/"><i class="fa fa-google-plus"></i></a>
+            <a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
+        </div>
+    </div>
+    <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="JS/menu.js"></script>
+</body>
+
+</html>
